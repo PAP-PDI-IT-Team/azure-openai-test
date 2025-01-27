ea98bcddfcbda419dbb938bf02523f680e2a3d78
rigelprofiles <137376803+rigelprofiles@users.noreply.github.com>
2025-01-27 11:18:44 +0800

    updated assistants

--- app/Http/Controllers/AiController.php
+++ app/Http/Controllers/AiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use OpenAI;
+// use OpenAI\Laravel\Facades\OpenAI;
 use OpenAI\Transporters\GuzzleTransporter;  
 use GuzzleHttp\Client as GuzzleClient;  
 
@@ -19,45 +20,6 @@ class AiController extends Controller
 
         $prompt = $request->post('prompt');
 
-        $client = new GuzzleClient([
-            'base_uri' => "https://{$resource}.openai.azure.com/openai/deployments/{$deployment_id}/chat/completions?api-version={$ai_version}",
-            'headers' => [
-                'api-key' => $key,
-                'Content-Type' => 'application/json'
-            ]
-        ]);
-        
-        try {
-            $response = $client->post('', [
-                'json' => [
-                    'messages' => [
-                        ['role' => 'user', 'content' => $prompt]
-                    ]
-                ]
-            ]);
-
-            $result = json_decode($response->getBody(), true);
-
-            $message = $result['choices'][0]['message']['content'] ?? 'No message found';
-
-            logger($message);
-            return response()->json(['data' => $message]);
-        } catch (\Exception $error) {
-            return response()->json(['error' => $error->getMessage()], 500);
-        }
-    }
-
-    public function thread(Request $request){
-        $key = config('app.azure_key');
-        $resource = config('app.azure_resource');
-        $deployment_id = config('app.azure_deployment_id');
-        $ai_version = config('app.azure_ai_version');
-
-        // $assistantId = $request->post('assistant_id');
-        $assistantId = 'asst_Y2wli5vbneJrdbYT8XKYKLxH';
-        $prompt = $request->post('prompt');
-
-        // $baseUri = "https://{$resource}.openai.azure.com/openai/deployments/{$deployment_id}/chat/completions?api-version={$ai_version}";
         $baseUri = "https://{$resource}.openai.azure.com/openai/deployments/{$deployment_id}";
         try {
             $client = OpenAI::factory()
@@ -69,7 +31,7 @@ class AiController extends Controller
             
             $result = $client->chat()->create([
                 'messages' => [
-                    ['role' => 'user', 'content' => 'Hi.'],
+                    ['role' => 'user', 'content' => $prompt],
                 ],
             ]); 
             logger($result['choices'][0]['message']['content']);
@@ -77,86 +39,107 @@ class AiController extends Controller
         } catch(\Exception $error) {
             return response()->json(['error' => $error->getMessage()], 500);
         }
-        // logger($baseUri);
+    }
 
+    public function thread(Request $request){
+        $key = config('app.azure_key');
+        $resource = config('app.azure_resource');
+        $deployment_id = config('app.azure_deployment_id');
+        $ai_version = config('app.azure_ai_version');
+
+        // $assistantId = $request->post('assistant_id');
+        $assistantId = 'asst_xVM002Vbb72Rb32i5LDc8fgc';
+        $prompt = $request->post('prompt');
+
+        // $baseUri = "https://{$resource}.openai.azure.com/openai/threads?api-version=2024-05-01-preview";
+        $baseUri = "https://azure-test-ai-rigel.openai.azure.com/";
+        logger($baseUri);
+        try {
+             $createThread= Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'api-key' => $key, 
+                ])->post("https://{$resource}.openai.azure.com/openai/threads?api-version=2024-05-01-preview", [])
+                ->json();
+
+            if ($createThread) {
+                logger('Thread created');
+                $threadId = $createThread['id'] ?? null;
+                
+                $response = $this->sendThreadMessage($key, $resource, $threadId, $prompt);
+                $messageId = $response['id'];
+                logger($messageId);
+                
+                $run = $this->runThread($key, $resource, $threadId, $assistantId);
+
+                if($run === 'completed') {
+                    $threadMessage = $this->getThreadMessages($key, $resource, $threadId, $messageId);
+                    $message = $threadMessage['data'][0]['content'][0]['text']['value'];
+                    logger($message);
+                }
+                return response()->json(['message' => $message], 200);
+            }
         
-        // $client = OpenAI::factory()
-        // ->withBaseUri("https://{$resource}.openai.azure.com/")
-        // ->withHttpClient(new \GuzzleHttp\Client(['timeout' => 300, 'connect_timeout' => 300]))
-        // ->withHttpHeader('api-key', $key)
-        // ->withQueryParam('api-version', $ai_version)
-        // ->make();
+        } catch(\Exception $error) {
+            logger($error);
+            return response()->json(['error' => $error->getMessage()], 500);
+        }
 
-        // $createThread= Http::withHeaders([
-        //     'Content-Type' => 'application/json',
-        //     'api-key' => $key, 
-        // ])->post("https://{$resource}.openai.azure.com/openai/threads?api-version=2024-05-01-preview", [])
-        //     ->json();
+    }
 
-        // if ($createThread) {
-        //     logger('Thread created');
-        //     $threadId = $createThread['id'] ?? null;
+    private function sendThreadMessage($key, $resource, $threadId, $message, ) {
+        $message = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'api-key' => $key, 
+        ])->post("https://{$resource}.openai.azure.com/openai/threads/{$threadId}/messages?api-version=2024-05-01-preview", [
+            'role' => 'user',
+            'content' => $message, 
+        ])->json();
+        return $message;
+    }
 
-        //     $message = Http::withHeaders([
-        //         'Content-Type' => 'application/json',
-        //         'api-key' => $key, 
-        //     ])->post("https://{$resource}.openai.azure.com/openai/threads/{$threadId}/messages?api-version=2024-05-01-preview", [
-        //         'role' => 'user',
-        //         'content' => $prompt, 
-        //     ])->json();
+    private function runThread($key, $resource, $threadId, $assistantId) {
+        logger('Running thread');
+        $runThread = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'api-key' => $key, 
+        ])->post("https://{$resource}.openai.azure.com/openai/threads/{$threadId}/runs?api-version=2024-05-01-preview", [
+            'assistant_id' => $assistantId,
+        ])->json();
 
-        //     $runThread = Http::withHeaders([
-        //         'Content-Type' => 'application/json',
-        //         'api-key' => $key, 
-        //     ])->post("https://{$resource}.openai.azure.com/openai/threads/{$threadId}/runs?api-version=2024-05-01-preview", [
-        //         'assistant_id' => $assistantId,
-        //     ])->json();
+        logger('run1:', ['id' => $runThread['id']]);
 
-        //     logger($runThread);
+        if($runThread) {
+            $runId = $runThread['id'] ?? null;
+            logger('run2:', ['id' => $runThread['id']]);
+            $status = 'queued';
 
-        //     if($runThread) {
-        //         $runId = $runThread['id'] ?? null;
-        //         $statusThread = Http::withHeaders([
-        //             'Content-Type' => 'application/json',
-        //             'api-key' => $key, ])->post("https://{$resource}.openai.azure.com/openai/threads/${threadId}/runs/${runId}?api-version=2024-05-01-preview", [])
-        //             ->json();
-        //         logger($statusThread);
-        //     }
+            while($status !== 'completed') {
+                $status = $this->checkThreadStatus($key, $resource, $threadId, $runId);
+                sleep(5);
+            }
+            logger("Thread run has completed successfully.");
+            return $status;
+        }
 
+    }
 
-        // } else {
-        //     logger('Failed to create thread');
-        // }
+    private function checkThreadStatus($key, $resource, $threadId, $runId) {
+        logger('Checking thread status');
+        $statusThread = Http::withHeaders([
+            'api-key' => $key, ])->post("https://{$resource}.openai.azure.com/openai/threads/{$threadId}/runs/{$runId}?api-version=2024-05-01-preview", [])
+            ->json();
+        return $statusThread['status'] ?? 'in_progress';
+    }
 
-        // $client = new GuzzleClient([
-        //     'base_uri' => "https://{$resource}.openai.azure.com/openai/assistants?api-version=2024-05-01-preview",
-        //     'headers' => [
-        //         'api-key' => $key,
-        //         'Content-Type' => 'application/json'
-        //     ]
-        // ]);
+    private function getThreadMessages($key, $resource, $threadId, $messageId) {
+        logger('Getting thread messages');
 
-        // $thread_response =  $openAI::threads()->create([]);
-        // $thread = $thread_response->toArray(); 
-        // $message_response =  $openAI::threads()->messages()->create($thread['id'], [
-        //     'role' => 'user',
-        //     'content' =>  $prompt, 
-        // ]);
-        // $message = $message_response->toArray(); 
-       
-        // $run_response =  $openAI::threads()->runs()->create(
-        //     threadId: $thread['id'], 
-        //     parameters: [
-        //         'assistant_id' => 'asst_Y2wli5vbneJrdbYT8XKYKLxH',
-        //     ],
-        // );
-        // $run = $run_response->toArray();
-
-        // return response()->json([
-        //     'thread_id' => $thread['id'], 
-        //     'message_id' => $message['id'], 
-        //     'run_id' => $run['id']
-        // ], 200);
+        $messages = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'api-key' => $key, ])->get("https://{$resource}.openai.azure.com/openai/threads/{$threadId}/messages?api-version=2024-05-01-preview")
+            ->json();
+        
+        return $messages;
     }
    
 }
