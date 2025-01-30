2f6e3e885d243d5dadeb74144a8f377a0f40cfca
Kyle Emmanuel <143974311+KyleEmmanuel@users.noreply.github.com>
2025-01-30 09:58:15 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('viewApiDocs', function (User $user) {
-            return true;
-        });
+        Schema::defaultStringLength(191);
+        
+        // Gate::define('viewApiDocs', function (User $user) {
+        //     return true;
+        // });
     }
 }
