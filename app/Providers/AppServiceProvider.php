b4a1ac001cdd6fa45ac35ee8157cd5e532545398
Kyle Emmanuel <143974311+KyleEmmanuel@users.noreply.github.com>
2025-01-31 15:53:14 +0800

    Conditional force scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +29,9 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('viewApiDocs', function (User $user) {
             return true;
         });
+
+        if (!in_array(Request::getHost(), ['localhost', '127.0.0.1'])) {
+            URL::forceScheme('https');
+        }
     }
 }
