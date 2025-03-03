cdafffc5578e2fea184ffef61ea0f9f3f3e40b75
Kyle Emmanuel <143974311+KyleEmmanuel@users.noreply.github.com>
2025-03-03 08:53:39 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://mindwell.assessmentcenter.ph'],
 
     'allowed_origins_patterns' => [],
 
